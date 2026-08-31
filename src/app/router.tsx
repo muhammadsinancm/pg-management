aaf357c5bf7aceebc14f358d2c1fec7b70ff9015
Muhammad Sinan C M <muhammadsinancm30@gmail.com>
2026-09-01 00:35:22 +0530

    new route added, for floor

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,7 +3,6 @@ import { LoginPage } from '@/features/auth/pages/LoginPage'
 import { DashboardShell } from '@/features/dashboard/components/DashboardShell'
 import { DashboardPage } from '@/features/dashboard/pages/DashboardPage'
 import { ProtectedRoute } from './ProtectedRoute'
-import { RoomsPage } from '@/features/pg/rooms/pages/RoomsPage'
 import { RoomDetailsPage } from '@/features/pg/rooms/pages/RoomDetailsPage'
 import { PaymentDetailsPage } from '@/features/pg/payments/pages/PaymentDetailsPage'
 import { PaymentCreatePage } from '@/features/pg/payments/pages/PaymentCreatePage'
@@ -13,6 +12,8 @@ import { BookingsPage } from '@/features/pg/bookings/components/BookingsPage'
 import { BookingDetailsPage } from '@/features/pg/bookings/pages/BookingDetailsPage'
 import { BookingCreatePage } from '@/features/pg/bookings/pages/BookingCreatePage'
 import { ReportsPage } from '@/features/pg/reports/pages/ReportsPage'
+import { FloorRoomsPage } from '@/features/pg/rooms/pages/FloorRoomsPage'
+import { FloorsPage } from '@/features/pg/rooms/pages/FloorsPage'
 
 export function AppRouter(): React.JSX.Element {
   return (
@@ -23,16 +24,18 @@ export function AppRouter(): React.JSX.Element {
         <Route element={<ProtectedRoute />}>
           <Route element={<DashboardShell />}>
             <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path='/pg/rooms' element={<RoomsPage />} />
+            <Route path='/pg/rooms' element={<FloorsPage />} />
+            <Route path='/pg/rooms/floor/:floorId' element={<FloorRoomsPage />} />
+            <Route path='/pg/rooms/:floorId/:roomId' element={<RoomDetailsPage />} />
             <Route path='/pg/payments' element={<PaymentsPage />} />
             <Route path='/pg/rooms/:roomId' element={<RoomDetailsPage />} />
             <Route path='/pg/payments/create' element={<PaymentCreatePage />} />
             <Route path='/pg/payments/:paymentId' element={<PaymentDetailsPage />} />
-            <Route path='/pg/payments/:paymentId/receipt' element={<ReceiptPage/>}/>
-            <Route path='/pg/bookings' element={<BookingsPage/>}/>
-            <Route path='/pg/bookings/create' element={<BookingCreatePage/>}/>
-            <Route path='/pg/bookings/:bookingId' element={<BookingDetailsPage/>}/>
-            <Route path='/pg/reports' element={<ReportsPage/>}/>
+            <Route path='/pg/payments/:paymentId/receipt' element={<ReceiptPage />} />
+            <Route path='/pg/bookings' element={<BookingsPage />} />
+            <Route path='/pg/bookings/create' element={<BookingCreatePage />} />
+            <Route path='/pg/bookings/:bookingId' element={<BookingDetailsPage />} />
+            <Route path='/pg/reports' element={<ReportsPage />} />
           </Route>
         </Route>
 
